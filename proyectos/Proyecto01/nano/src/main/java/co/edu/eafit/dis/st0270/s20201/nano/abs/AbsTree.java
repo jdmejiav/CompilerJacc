61ae0075078058af662c705ce90d207917298794
Armando Ríos Gallego <53227034+armando555@users.noreply.github.com>
2020-06-01 07:48:53 +0000

    Version inicial del proyecto

--- proyectos/Proyecto01/nano/src/main/java/co/edu/eafit/dis/st0270/s20201/nano/abs/AbsTree.java
+++
@@ -1,7 +0,0 @@
-package co.edu.eafit.dis.st0270.s20201.nano.abs;
-
-import  co.edu.eafit.dis.st0270.s20201.nano.visitor.Visitor;
-
-public abstract class AbsTree implements Element {
-   public abstract void accept(Visitor visitor);
-}
--- proyectos/Proyecto01/nano/src/main/java/co/edu/eafit/dis/st0270/s20201/nano/visitor/Visitor.java
+++
@@ -1,9 +0,0 @@
-package co.edu.eafit.dis.st0270.s20201.nano.visitor;
-
-import co.edu.eafit.dis.st0270.s20201.nano.abs.DyckAbs;
-import co.edu.eafit.dis.st0270.s20201.nano.abs.ProgAbs;
-
-public interface Visitor {
-   void visit(ProgAbs element);
-   void visit(DyckAbs element);
-}
