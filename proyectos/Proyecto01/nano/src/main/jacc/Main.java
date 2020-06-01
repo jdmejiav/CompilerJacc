de91fdb1892c54ddee9623d72b8045201ae7544e
ariosg <ariosg@e0f5ec30-9513-4265-b74f-5f5563aaea89>
2020-06-01 21:46:56 +0000

    Automata automático que compila pero no ejecuta :c

---
+++ proyectos/Proyecto01/nano/src/main/jacc/Main.java
@@ -0,0 +1,26 @@
+ import java.io.FileReader;
+ import java.io.FileNotFoundException;
+ public class Main{
+    //usage java main filenames ...
+    public static void main(String args[]){
+        for (int i=0;i<args.length;i++){
+            try{
+                FileReader fr=new FileReader(args[i]);
+                CfglrLexer dl=new CfglrLexer(fr);
+                CfglrParser dp=new CfglrParser(dl);
+                dl.nextToken();
+                if(dp.parse()){
+                    System.out.println(""+args[i]+
+                                       " está bien formado");
+                }else{
+                    System.out.println(""+args[i]+
+                                       " no está bien formado");
+                }
+                
+            }
+            catch(FileNotFoundException fnfe){
+                System.err.println("Error: "+ args[i] + " no existe");
+            }
+        }
+    }
+ }
