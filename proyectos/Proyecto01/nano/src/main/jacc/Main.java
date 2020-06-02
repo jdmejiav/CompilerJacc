dc004acff759c04fc717e62b794d3dcfcb369a11
ariosg <ariosg@e0f5ec30-9513-4265-b74f-5f5563aaea89>
2020-06-02 18:04:31 +0000

    Actualizado automata automatico

--- proyectos/Proyecto01/nano/src/main/jacc/Main.java
+++ proyectos/Proyecto01/nano/src/main/jacc/Main.java
@@ -1,25 +1,27 @@
- import java.io.FileReader;
- import java.io.FileNotFoundException;
- public class Main{
-    //usage java main filenames ...
-    public static void main(String args[]){
-        for (int i=0;i<args.length;i++){
-            try{
-                FileReader fr=new FileReader(args[i]);
-                CfglrLexer dl=new CfglrLexer(fr);
-                CfglrParser dp=new CfglrParser(dl);
-                if(dp.parse()){
-                    System.out.println(""+args[i]+
-                                       " está bien formado");
-                }else{
-                    System.out.println(""+args[i]+
-                                       " no está bien formado");
-                }
-                
-            }
-            catch(FileNotFoundException fnfe){
-                System.err.println("Error: "+ args[i] + " no existe");
-            }
-        }
-    }
- }
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+
+public class Main{
+
+	//usage java Main <filenames>
+	public static void main (String [] args){
+		for (int i =0;i<args.length;i++){
+
+			try{
+				FileReader fr = new FileReader(args[i]);
+				NanoLexer dl = new NanoLexer(fr);
+				NanoParser dp = new NanoParser(dl);
+				dl.nextToken();
+				if (dp.parse()){
+					System.out.println(args[i]+" está bien formado");
+				}else {
+					System.out.println(args[i]+" no está bien formado");
+				}
+			}catch (FileNotFoundException fnfe){
+				System.err.println("Erro: "+args[i]+" no existe");
+			}
+
+		}
+
+	}
+}
