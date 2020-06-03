475317f43250c2b28a5571a1b7c1ae7d35d6ddc7
ariosg <ariosg@e0f5ec30-9513-4265-b74f-5f5563aaea89>
2020-06-03 21:46:06 +0000

    Borre el proyecto

--- proyectos/Proyecto01/nano/src/main/java/co/edu/eafit/dis/st0270/s20201/nano/grammar/EnumGrammarSymbol.java
+++
@@ -1,5 +0,0 @@
-package co.edu.eafit.dis.st0270.s20201.nano.grammar;
-
-public enum EnumGrammarSymbol {
-    S0, S, A, B , S1 , C , Asignacion, Coma , Palito , Min , May , I0, I1, I2, I3, I4, I5, I6, I7, I8, I9, I10 ,I11 ,I12 ,I13 ,I14 ,I15 ,I16 , EndOfString
-}
