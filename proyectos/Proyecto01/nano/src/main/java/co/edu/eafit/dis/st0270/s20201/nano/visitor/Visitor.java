094ca1d2ec79f5cb30a7d3284be68818fd19931d
Armando Ríos Gallego <53227034+armando555@users.noreply.github.com>
2020-06-03 21:46:06 +0000

    Borre el proyecto

--- proyectos/Proyecto01/nano/src/main/java/co/edu/eafit/dis/st0270/s20201/nano/visitor/Visitor.java
+++
@@ -1,9 +0,0 @@
-package co.edu.eafit.dis.st0270.s20201.nano.visitor;
-
-import co.edu.eafit.dis.st0270.s20201.nano.abs.CfglrAbs;
-import co.edu.eafit.dis.st0270.s20201.nano.abs.ProgAbs;
-
-public interface Visitor {
-   void visit(ProgAbs element);
-   void visit(CfglrAbs element);
-}
